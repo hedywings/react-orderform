3b6c0da4255560821f8f3415edec3ae905bba81c
Hedy Wang <h8108170@gmail.com>
2021-03-15 21:27:19 +0800

    Implement Order model

--- src/models/order.js
+++ src/models/order.js
@@ -1,28 +1,97 @@
 // import axios from 'axios';
 
 const initialState = {
-  orderList: [],
+    orderList: null,
+    errorMessage: null
 }
 
 const orderModel =  {
-  namespace: 'order',
+    namespace: 'order',
 
-  /**
-   *  Initial state
-   */
-  state: initialState,
+    /**
+     *  Initial state
+     */
+    state: initialState,
 
-  /**
-   * Effects/Actions
-   */
-  effects: (dispatch) => ({
-  }),
+    /**
+     * Effects/Actions
+     */
+    effects: (dispatch) => ({
+        async getOrderList(payload, rootState) {
+            try {
+                // const response = await axios({
+                //     method: 'get',
+                //     url: '/api//orderform/list',
+                //     params: { ...payload, rpp: 10 },
+                // });
+                // dispatch.order.getOrderListSuccess(response.data);
 
-  /**
-   * Reducers
-   */
-  reducers: {
-  },
+                dispatch.order.getOrderListSuccess([
+                    {
+                        name: 'Livi優活 抽取式衛生紙(100抽x10包x10串/箱)',
+                        logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
+                        status: {
+                          code: 3,
+                          type: '已取消'
+                        },
+                        date: '107/6/12'
+                    },
+                    {
+                        name: 'BALMUDA The Toaster 百慕達烤麵包機-黑色',
+                        logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
+                        status: {
+                          code: 2,
+                          type: '已成立'
+                        },
+                        date: '108/7/21'
+                    },
+                    {
+                        name: '贈-短慧萬用鍋HD2133+三合一濾網「LG樂金」韓國原裝...',
+                        logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
+                        status: {
+                          code: 1,
+                          type: '處理中'
+                        },
+                        date: '108/6/2'
+                     },
+                     {
+                        name: 'Apple AirPds 2',
+                        logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
+                        status: {
+                          code: 4,
+                          type: '已送達'
+                        },
+                        date: '108/3/02'
+                    }
+                ]);
+            } catch(error) {
+                if (!error.response || !error.response.status) {
+                    return;
+                }
+
+                const errorMessage = error.response.status + ' Error';
+                dispatch.order.getOrderListFail(errorMessage);
+            }
+        }
+    }),
+
+    /**
+     * Reducers
+     */
+    reducers: {
+        getOrderListSuccess(state, payload) {
+            return Object.assign({}, state, {
+                orderList: payload
+            });
+        },
+        getOrderListFail(state, payload) {
+            return Object.assign({}, state, {
+                orderList: [],
+                errorMessage: payload
+            });
+        },
+
+    },
 };
 
 export default orderModel;
--- src/screens/OrderForm.js
+++ src/screens/OrderForm.js
@@ -1,13 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { makeStyles } from '@material-ui/styles';
+import { CircularProgress } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    progressDiv: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '100vh'
+    }
+}));
 
 function OrderForm (props) {
+    const classes = useStyles(props);
+    const dispatch = useDispatch();
+    const orderList = useSelector(({ order }) => order.orderList);
+    const orderErrorMessage = useSelector(({ order }) => order.errorMessage);
 
-  return (
-      <div>
-          <h1>Hello</h1>
-      </div>
-  )
+    useEffect(()=>{
+        dispatch.order.getOrderList();
+    }, [dispatch]);
+
+    if (!orderList) {
+        return (
+            <div className={classes.progressDiv}>
+                <CircularProgress />
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
 };
 
 export default OrderForm;
